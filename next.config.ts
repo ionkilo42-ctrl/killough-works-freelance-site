37db9cbebf578ee78b454595e41587a1cce9026f
Jonathan Killough <ionkilo42@gmail.com>
2026-05-27 17:40:16 -0400

    Redirect www to apex domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.killough.works",
+          },
+        ],
+        destination: "https://killough.works/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
